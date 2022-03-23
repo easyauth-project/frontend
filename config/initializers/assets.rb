740d543a1f401a8e8faec0d8e86a137276db2655
JeffGThompson <jeffgthompson@gmail.com>
2022-03-23 12:04:15 -0400

    Add files via upload

---
+++ config/initializers/assets.rb
@@ -0,0 +1,33 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( easyauth.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min )
+Rails.application.config.assets.precompile += %w( bootstrap )
+Rails.application.config.assets.precompile += %w( jquery-1.10.2 )
+Rails.application.config.assets.precompile += %w( org/pkijs/csr )
+
+Rails.application.config.assets.precompile += %w( org/pkijs/common )
+Rails.application.config.assets.precompile += %w( org/pkijs/asn1 )
+Rails.application.config.assets.precompile += %w( org/pkijs/x509_schema )
+Rails.application.config.assets.precompile += %w( org/pkijs/x509_simpl )
+
+
+
+
+
